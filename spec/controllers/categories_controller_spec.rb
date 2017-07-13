5cb3924107d28d7beb9f31c0600088162f2437cf
MikelSage <michael.sagapolutele@yahoo.com>
2017-07-13 16:54:58 -0600

    add controller test for invalid params

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
-
+  describe 'POST #create' do
+    context 'with invalid params' do
+      it "renders the 'new' template" do
+        post :create, params: {category: attributes_for(:category, title: nil)}
+        expect(response).to render_template('new')
+      end
+    end
+  end
 end
